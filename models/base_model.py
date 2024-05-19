c22272c7079e6ea19fc5e9a337812968e8b0d974
eslam <eslam@org.com>
2024-05-19 18:32:18 -0400

    mm

--- models/base_model.py
+++ models/base_model.py
@@ -1,60 +1,70 @@
 #!/usr/bin/python3
-"""this is the base model."""
-
-from uuid import uuid4
+"""This module defines a base class for all models in our hbnb clone"""
+import uuid
 from datetime import datetime
-import models
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, DateTime
+
+Base = declarative_base()
 
 
 class BaseModel:
-    """BaseModel class."""
+    """A base class for all hbnb models"""
+    id = Column(String(60), unique=True,
+                nullable=False, primary_key=True)
+    created_at = Column(
+        DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(
+        DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instance Constructor.
-
-        args
-        id:unique identifier.
-        created_at: date created at.
-        updated_at: date updated at.
-        """
+        """Instatntiates a new model"""
         if not kwargs:
-            self.id = str(uuid4())
+            self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = datetime.now()
-            models.storage.new(self)
+            self.updated_at = self.created_at
+        elif 'id' not in kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+            self.__dict__.update(kwargs)
         else:
-            for k, v in kwargs.items():
-                if k != "__class__":
-                    if k == "updated_at":
-                        self.updated_at = datetime.fromisoformat(v)
-                    elif k == "created_at":
-                        self.created_at = datetime.fromisoformat(v)
-                    else:
-                        setattr(self, k, v)
+            kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
+                                                     '%Y-%m-%dT%H:%M:%S.%f')
+            del kwargs['__class__']
+            self.__dict__.update(kwargs)
 
     def __str__(self):
-        """__str__.
-
-        return string Representation.
-        """
-        return f"[{self.__class__.__name__}] ({self.id}) <{self.__dict__}>"
+        """Returns a string representation of the instance"""
+        new_dict = self.__dict__.copy()
+        if '_sa_instance_state' in new_dict:
+            del new_dict['_sa_instance_state']
+        cls = (str(type(self)).split('.')[-1]).split('\'')[0]
+        return '[{}] ({}) {}'.format(cls, self.id, new_dict)
 
     def save(self):
-        """save.
-
-        update atr updated_at
-        """
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
         self.updated_at = datetime.now()
-        models.storage.save()
+        storage.new(self)
+        storage.save()
+
+    def delete(self):
+        """Updates updated_at with current time when instance is changed"""
+        from models import storage
+        storage.delete(self)
 
     def to_dict(self):
-        """To_dict.
+        """Convert instance into dict format"""
+        dictionary = {}
+        dictionary.update(self.__dict__.copy())
+        if '_sa_instance_state' in dictionary:
+            del dictionary['_sa_instance_state']
+        dictionary.update({'__class__':
+                          (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary['created_at'] = self.created_at.isoformat()
+        dictionary['updated_at'] = self.updated_at.isoformat()
 
-        Returns:
-            dict: dictionary representation.
-        """
-        my_class_dict = self.__dict__.copy()
-        my_class_dict["__class__"] = self.__class__.__name__
-        my_class_dict["updated_at"] = self.updated_at.isoformat()
-        my_class_dict["created_at"] = self.created_at.isoformat()
-        return my_class_dict
+        return dictionary
